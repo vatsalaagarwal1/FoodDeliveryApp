ab04a63947a5822c78efa2208b642276bd067d77
Vatsala Agarwal <86197185+vatsalaagarwal1@users.noreply.github.com>
2023-07-23 13:31:39 +0530

    check

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+
+
+  return (
+      <View>
+        <Text>Heloooooo</Text>
+      </View>
+  );
+}
+
+
+
+export default App;
